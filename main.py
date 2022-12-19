011cfe65e775de11abf7dbe1505165de5ef8b9f7
Clay-the-Great <106385441+Clay-the-Great@users.noreply.github.com>
2022-12-19 10:55:09 +0800

    Add custom watermark text

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def add_watermark():
         draw = ImageDraw.Draw(image)
 
         # create watermark
-        text = WATERMARK_TEXT
+        text = mark_entry.get()
         font = ImageFont.truetype('arial.ttf', 20)
         # text_width, text_height = draw.textsize(text, font)
         (left, bottom, text_width, text_height) = draw.textbbox(xy=(0, 0), text=text, font=font)
@@ -62,6 +62,7 @@ def add_watermark():
         image.show()
         global marked_image
         marked_image = image
+        mark_entry.delete(0, "end")
 
 
 # Save watermarked image
@@ -91,7 +92,7 @@ window.title("Watermarking")
 window.config(pady=50, padx=100, bg=GREY)
 
 heading_label = tkinter.Label(
-    text="Add a watermark to the picture of your choice",
+    text="Watermark An Image of Your Choice",
     bg=GREY, fg=DARK_BLUE, font=(FONT_NAME, 20, "bold")
 )
 heading_label.grid(column=0, row=0, columnspan=5)
@@ -102,22 +103,30 @@ canvas.create_image(32, 32, image=coffee_img)
 canvas.grid(column=0, row=1, columnspan=5, pady=(40, 0))
 
 file_label = tkinter.Label(text="", bg=GREY)
-file_label.config(pady=40)
+file_label.config(pady=20)
 file_label.grid(column=0, row=2, columnspan=5)
 
-load_button = tkinter.Button(text="Load Image", command=load_image)
-load_button.grid(column=0, row=3)
+mark_label = tkinter.Label(text="Watermark Text:", bg=GREY)
+mark_label.config(pady=15)
+mark_label.grid(column=0, row=3)
 
-show_button = tkinter.Button(text="Show Image", command=show_image)
-show_button.grid(column=1, row=3)
+mark_entry = tkinter.Entry(width=32)
+mark_entry.grid(column=1, row=3, columnspan=2, pady=(15, 15))
+mark_entry.focus()
 
-watermark_button = tkinter.Button(text="Add Watermark", command=add_watermark)
-watermark_button.grid(column=2, row=3)
+load_button = tkinter.Button(text="Load Image", command=load_image, width=13)
+load_button.grid(column=0, row=4)
+
+show_button = tkinter.Button(text="Show Image", command=show_image, width=11)
+show_button.grid(column=1, row=4)
+
+watermark_button = tkinter.Button(text="Add Watermark", command=add_watermark, width=14)
+watermark_button.grid(column=3, row=3)
 
 save_button = tkinter.Button(text="Save Watermarked", command=save)
-save_button.grid(column=3, row=3)
+save_button.grid(column=2, row=4)
 
-directory_button = tkinter.Button(text="Open Directory", command=open_directory)
-directory_button.grid(column=4, row=3)
+directory_button = tkinter.Button(text="Open Directory", command=open_directory, width=14)
+directory_button.grid(column=3, row=4)
 
 window.mainloop()
